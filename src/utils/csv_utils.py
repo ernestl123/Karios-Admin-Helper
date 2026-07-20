c6909419a0370f001e63084ae8a63d725faa4916
ernestl123 <ernestl8502@gmail.com>
2026-07-19 17:46:28 -0700

    Readded utils

---
+++ src/utils/csv_utils.py
@@ -0,0 +1,61 @@
+import csv
+import discord
+
+def read_csv(file_path: str):
+    """
+    Reads a CSV file and returns its contents as a list of dictionaries.
+
+    Args:
+        file_path (str): The path to the CSV file.
+    """
+
+    with open(file_path, mode='r', newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        return [row for row in reader]
+    
+
+async def mass_migrate_csv(file_path: str, guild: discord.Guild):
+    """
+    Args:
+        file_path (str): The path to the CSV file.
+        guild (discord.Guild): The guild to migrate users to.
+
+    Returns:
+        str: A summary of the migration process.
+    """
+
+    reader = read_csv(file_path)
+    output = ""
+
+    for row in reader:
+        name = row.get('Name')
+        teams = row.get('Teams').split(', ') if row.get('Teams') else []
+
+        teams.append("Leadership")
+        print(f"Name: {name}, Teams: {teams}")
+
+        member = discord.utils.find(
+            lambda m: m.nick and m.nick.lower() == name.lower(), 
+            guild.members
+        )
+        if not member:
+            output += f"🛑Member with nickname '{name}' not found in guild '{guild.name}'. Manual assignment required.\n"
+            continue
+            
+        # Assign roles to the member given each team in teams
+        for team in teams:
+            if team == "Life Group":
+                team = "Life Group Leader"
+                
+            role = discord.utils.get(guild.roles, name=team)
+            if not role:
+                output += f"🛑Role '{team}' not found in guild '{guild.name}'. Manual assignment required.\n"
+                continue
+            
+            try:
+                await member.add_roles(role, reason=f"Assigning role '{team}' to member '{member.name}' from CSV migration")
+                print(f"Assigned role '{team}' to member '{member.name}'.")
+            except Exception as e:
+                output += f"🛑Failed to assign role '{team}' to member. Manual assignment required for '{member.name}': {e}\n"
+
+    return output
---
+++ src/utils/role_utils.py
@@ -0,0 +1,86 @@
+import discord
+
+async def give_role(member: discord.Member, role_name: int):
+    """
+    Assigns a role to a member by role name.
+    Args:
+        member (discord.Member): The member to assign the role to.
+        role_name (int): The name of the role to assign.
+    
+    Returns:
+        bool: True if the role was successfully assigned, False otherwise.
+    """
+    role = discord.utils.get(member.guild.roles, name=role_name)
+    if not role:
+        raise ValueError(f"Role '{role_name}' not found in guild '{member.guild.name}'.")
+    
+    try:
+        await member.add_roles(role, reason=f"Assigning role '{role_name}' to member '{member.name}'")
+        print(f"Assigned role '{role_name}' to member '{member.name}'.")
+        return True
+    except Exception as e:
+        raise ValueError(f"Failed to assign role '{role_name}' to member '{member.name}': {e}")
+    
+async def retire_role(role: discord.Role, grad_year: int):
+    """
+    Renames a role to indicate that it is retired for a specific graduation year and
+    switch color to gray
+    Args:
+        role (discord.Role): The role to rename.
+        grad_year (int): The last two digits of the graduation year to append to the role name.
+    """
+    new_role_name = f"{role.name} '{grad_year}"
+    try:
+        await role.edit(name=new_role_name, color=discord.Color.default(), reason=f"Transitioning role for graduation year '{grad_year}")
+        print(f"Edited role '{role.name}' to '{new_role_name}'.")
+        return True
+    except Exception as e:
+        raise ValueError(f"Failed to edit role '{role.name}': {e}")
+
+async def duplicate_role(role: discord.Role):
+    """
+    Duplicates a role
+
+    Args:
+        role (discord.Role): The role to duplicate.
+
+    Returns:
+        discord.Role: The newly created duplicated role.
+    """
+    try:
+        return await role.guild.create_role(name=role.name, color=role.color, reason=f"Copying role '{role.name}' for new year")
+    except Exception as e:
+        raise ValueError(f"Failed to copy role '{role.name}': {e}")
+
+async def transition_role(role: discord.Role, grad_year: str):
+    """
+    Transitions a role to a new graduation year by retiring the old role and creating a new one.
+
+    Args:
+        role (discord.Role): The role to transition.
+        grad_year (str): The graduation year to append to the new role name.
+    """
+    new_role = await duplicate_role(role)
+    await retire_role(role, grad_year)
+    return new_role
+
+async def give_role(member: discord.Member, role_name: str):
+    """
+    Assigns a role to a member by role name.
+    Args:
+        member (discord.Member): The member to assign the role to.
+        role_name (str): The name of the role to assign.
+    
+    Returns:
+        bool: True if the role was successfully assigned, False otherwise.
+    """
+    role = discord.utils.get(member.guild.roles, name=role_name)
+    if not role:
+        raise ValueError(f"Role '{role_name}' not found in guild '{member.guild.name}'.")
+    
+    try:
+        await member.add_roles(role, reason=f"Assigning role '{role_name}' to member '{member.name}'")
+        print(f"Assigned role '{role_name}' to member '{member.name}'.")
+        return True
+    except Exception as e:
+        raise ValueError(f"Failed to assign role '{role_name}' to member '{member.name}': {e}")
